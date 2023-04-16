55948d590be408858b986e99ecc4a887be13385c
Surin Shah <76935531+SurinShah@users.noreply.github.com>
2023-04-16 08:43:01 +0530

    Final update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route}  from 'react-router-dom';
 import Start from './pages/Start';
-import Expensetracker from './pages/Expensetracker';
-import Dashboard from './pages/Dashboard';
 import './App.css';
 
 function App() {
@@ -9,8 +7,6 @@ function App() {
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<Start />}></Route>
-          <Route path="/expensetracker" element={<Expensetracker/>}></Route>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
           </Routes>
     </BrowserRouter>
   );
